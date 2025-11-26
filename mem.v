efb04797e4113be9554128ee1f17129cb80f75dc
IshDeshpa <ishdeshpa@gmail.com>
2025-11-25 21:26:02 -0600

    implemented memory and testbench

---
+++ mem.v
@@ -0,0 +1,28 @@
+module mem
+#(
+    parameter DATA_WIDTH=32,
+    parameter ADDRESS_WIDTH=8
+)
+(
+    input wire clk,
+    input wire w_en,
+    input wire sel,
+    input wire [ADDRESS_WIDTH-1:0] address_bus,
+    inout wire [DATA_WIDTH-1:0] data_bus
+);
+    reg [DATA_WIDTH-1:0] memory [(2**ADDRESS_WIDTH)-1:0];
+    reg [DATA_WIDTH-1:0] d_out;
+
+    assign data_bus = (sel && !w_en)?d_out:'hZ;
+
+    always @(posedge clk) begin
+        if (sel) begin
+            if (w_en) begin
+                memory[address_bus] <= data_bus;
+            end else begin 
+                d_out <= memory[address_bus];
+            end
+        end
+    end
+
+endmodule
---
+++ mem_tb.v
@@ -0,0 +1,42 @@
+module tb_mem;
+    reg clk;
+    reg cs;
+    reg w_en;
+
+    reg [7:0] addr;
+    wire [31:0] data_bus;
+    reg [31:0] tb_data;
+
+    assign data_bus = (cs && w_en)?tb_data:32'bZ;
+
+    mem dut (
+        .clk(clk),
+        .w_en(w_en),
+        .sel(cs),
+        .address_bus(addr),
+        .data_bus(data_bus)
+    );
+
+    always #10 clk = !clk;
+
+    initial begin 
+        {clk, cs, w_en, addr} <= 0;
+
+        repeat(2) @(posedge clk);
+
+        for (integer i=0; i<2**8; i=i+1) begin 
+            addr <= i; w_en <= 1; cs <= 1; tb_data <= $urandom;
+            repeat(1) @(posedge clk)
+            $display("Writing %0d to %0d", tb_data, addr);
+        end
+
+        for (integer i=0; i<2**8; i=i+1) begin 
+            addr <= i; w_en <= 0; cs <= 1;
+            repeat(2) @(posedge clk) ;
+            $display("Reading %0d from %0d", data_bus, addr);
+        end
+
+        $finish;
+    end
+
+endmodule
